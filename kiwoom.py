837003510e45e842f618b8b08ca4945d3c1abee8
rong5026 <rong5026@naver.com>
2023-03-31 14:30:39 +0900

    [ADD] 예수금확인

--- kiwoom.py
+++ kiwoom.py
@@ -1,4 +1,6 @@
 import pythoncom
+from PyQt5.QtCore import QEventLoop
+
 import constants
 from PyQt5.QAxContainer import *
 
@@ -8,6 +10,10 @@ class Kiwoom:
         self.login = False
         self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.ocx.OnEventConnect.connect(self.on_event_connect)
+        self.ocx.OnReceiveTrData.connect(self.trdata_slot)
+
+        self.detail_account_info_event_loop = None
+
 
     # 로그인 메서드 호출
     def comm_connect(self):
@@ -49,9 +55,6 @@ class Kiwoom:
         result = self.ocx.dynamicCall("GetMasterLastPrice(QString)", code)
         return result
 
-    def get_chejan_data(self, value):
-        result = self.ocx.dynamicCall("GetChejanData(int)", value)
-        return result
 
     # SendOrder 호출
     def send_buy_order(self, account, item_code, quantity, price, trading_type):
@@ -125,6 +128,38 @@ class Kiwoom:
                                       list(order_params.values()), )
         return result
 
+    def get_detail_account_info(self, deposit_name, pw, pw_mc, search5):
+        print("계좌번호 및 비밀번호 등을 입력/서버에 요청")
+        self.ocx.dynamicCall("SetInputValue(QString,QString)", "계좌번호", "8043137211")
+        self.ocx.dynamicCall("SetInputValue(QString,QString)", "비밀번호", "0000")
+        self.ocx.dynamicCall("SetInputValue(QString,QString)", "비밀번호입력매체구분", "00")
+        self.ocx.dynamicCall("SetInputValue(QString,QString)", "조회구분", "2")
+        self.ocx.dynamicCall("CommRqData(QString,QString,int,QString)", "예수금상세현황요청", "opw00001", 0, "2000")
+        #self.detail_account_info_event_loop = QEventLoop()
+        #self.detail_account_info_event_loop.exec_()
+
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+    def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
+        # TR SLOT 만들기
+        '''
+        TR 요청을 받는 구역, slot임
+        :param sScrNo: 스크린 번호
+        :param sRQName: 내가 요청했을 때 지은 이름
+        :param sTrCode: 요청 ID, TR코드
+        :param sRecordName: 사용안함
+        :param sPrevNext: 다음 페이지가 있는지
+        :return:
+        '''
+
+        # 예수금 등 조회 하기
+        if sRQName == "예수금상세현황요청":
+            deposit = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "예수금")
+            print("예수금 %s" % int(deposit))
+
+            ok_deposit = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "출금가능금액")
+            print("출금가능금액 %s" % int(ok_deposit))
+            #self.detail_account_info_event_loop.exit()
     def changed_trading_type(self, name):
         if (name == "지정가"):
             return constants.LIMIT_PRICE_VALUE
@@ -146,3 +181,6 @@ class Kiwoom:
             return constants.CHANGE_SELL
         else:
             return constants.ERROR_CODE
+
+
+
--- main.py
+++ main.py
@@ -2,23 +2,40 @@ import sys
 
 import pythoncom
 from PyQt5.QAxContainer import *
+from PyQt5.QtCore import QEventLoop
 from PyQt5.QtWidgets import *
 
 from kiwoom import Kiwoom
 
-class MyWindow(QMainWindow):
+class MyWindow():
     def __init__(self):
         super().__init__()
         self.kiwoom = Kiwoom()
         self.kiwoom.comm_connect()
 
+        while True:
+            n = int(input("메뉴번호를 선택하세요 : "))
+            if n == 1:
+                buy_order = self.kiwoom.send_buy_order("8043137211", "005930", 1, 0, "시장가")
+                print("결과값 : ", buy_order)
+            elif n == 2:
+                sell_order = self.kiwoom.send_sell_order("8043137211", "005930", 1, 0, "시장가")
+                print("결과값 : ", sell_order)
+            elif n == 3:
+                self.start_test()
+            elif n == 4:
+                result = self.kiwoom.get_detail_account_info("8043137211","0000","00",2)
+                print(result)
+
+
+    def start_test(self):
         # tr 요청
         name = self.kiwoom.get_master_code_name("005930")
 
         connectState = self.kiwoom.get_connect_state()
         lastPrice = self.kiwoom.get_master_last_price("005930")
 
-        print("연결상태 : %d" %connectState)
+        print("연결상태 : %d" % connectState)
         print("유저정보")
         print("------------------------------")
         print("계좌 수 : " + self.kiwoom.get_login_info("ACCOUNT_CNT"))
@@ -28,18 +45,7 @@ class MyWindow(QMainWindow):
         print("------------------------------")
         print(name)
         print("------------------------------")
-        print("전일가 : %s" %lastPrice)
-
-
-
-        #buy_order = self.kiwoom.sendBuyOrder("8043137211", "005930", 1, 0, "시장가")
-        #print(buy_order)
-
-        #sell_order = self.kiwoom.sendSellOrder("8043137211", "005930", 1, 0, "시장가")
-        #print(sell_order)
-
-        #print(self.kiwoom.get_chejan_data(11))
-
+        print("전일가 : %s" % lastPrice)
 
 
 
@@ -47,5 +53,5 @@ class MyWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyWindow()
-    window.show()
+    #window.show()
     app.exec_()
