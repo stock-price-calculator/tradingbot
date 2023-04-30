3e1c865cfdddc92e7c5f99d47675e95b8aca1826
rong5026 <rong5026@naver.com>
2023-04-30 19:38:00 +0900

    Revert "[ADD] 분봉pandas 데이터 정리"
    This reverts commit 16161cd96adfb2704e383b9166bf5fefbdca5320.

--- market/reception.py
+++ market/reception.py
@@ -1,34 +1,28 @@
 import view
-from datetime import datetime
-from pandas import DataFrame
+
 
 class Kiwoom_Receive_Market_price:
 
     def __init__(self, main_kiwoom):
         self.Kiwoom = main_kiwoom
-
-    # 분봉차트 값 받기
-    def receive_minutes_chart_data(self, sTrCode, sRQName, sRecordName):
+    #분봉차트 값 받기
+    def receive_minutes_chart_data(self,sTrCode, sRQName, sRecordName):
         repeat = self.Kiwoom.get_repeat_cnt(sTrCode, sRQName)
 
         received_data = []
-        for i in range(5):
-            current_price = int(self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "현재가").strip())
-            volume = int(self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "거래량").strip())
-            open_price = int(self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "시가").strip())
-            high_price = int(self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "고가").strip())
-            low_price = int(self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "저가").strip())
+        for i in range(1):
+            current_price = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "현재가").strip()
+            volume = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "거래량").strip()
+            open_price = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "시가").strip()
+            high_price = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "고가").strip()
+            low_price = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "저가").strip()
             standard_minute = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "체결시간").strip()
-            standard_minute = datetime.strptime(standard_minute, "%Y%m%d%H%M%S")
-
-            received_data.append([standard_minute, current_price, open_price, high_price, low_price, volume])
-
-        dataframe = DataFrame(received_data, columns=['time','current','open','high','low','volume'])
-        print(dataframe)
-            # view.주식분봉차트조회요청(standard_minute, current_price, open_price, high_price, low_price, volume)
+            received_data.append([standard_minute,current_price,open_price,high_price,low_price,volume])
+            print(received_data)
+            #view.주식분봉차트조회요청(standard_minute, current_price, open_price, high_price, low_price, volume)
 
     # 일봉차트 값 받기
-    def receive_day_chart_data(self, sTrCode, sRQName, sRecordName):
+    def receive_day_chart_data(self,sTrCode, sRQName, sRecordName):
         repeat = self.Kiwoom.get_repeat_cnt(sTrCode, sRQName)
 
         for i in range(repeat):
@@ -38,7 +32,7 @@ class Kiwoom_Receive_Market_price:
             high_price = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "고가").strip()
             low_price = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "저가").strip()
             standard_day = self.Kiwoom.get_comm_data(sTrCode, sRecordName, i, "일자").strip()
-            standard_minute = datetime.strptime(standard_minute, "%Y%m%d%H%M%S")
+
             view.주식일봉차트조회요청(standard_day, current_price, open_price, high_price, low_price, volume)
 
     # 주봉차트 값 받기
