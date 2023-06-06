bd0b85c805183c0a9a8411c8d53b8d7817422dc1
rong5026 <rong5026@naver.com>
2023-06-06 16:24:20 +0900

    [ADD] flask restful api

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 import sys
-
 from PyQt5.QtWidgets import *
-
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
-
 import constants
 from kiwoom import Kiwoom
 from threading import Thread
-
-
 
 app = Flask(__name__)
 api = Api(app)
 
 kiwoom = None
-@app.route("/")
-def get_root():
-    return jsonify("hello")
+
+
+
+class Login(Resource):
+    def get(self):
+        global kiwoom
+
+        if not kiwoom:
+            kiwoom = Kiwoom()
+
+        kiwoom.connect_login()
+
+        return 0
 
 # 처음 로그인 요청
 @app.route("/login")
@@ -30,7 +35,6 @@ def get_login():
     kiwoom.connect_login()
 
     return jsonify(0)
-
 
 @app.route("/test")
 def start_test():
@@ -47,15 +51,19 @@ def start_test():
     else:
         return jsonify(name)
 
+# Flask 서버 실행
 def run_flask_app():
     app.run(debug=True)
 
+# Kiwoom 서버 실행
 def run_kiwoom_app():
     global kiwoom
 
     app1 = QApplication(sys.argv)  # QApplication 인스턴스 생성
     kiwoom = Kiwoom()
     app1.exec_()
+
+api.add_resource(Login, '/login')
 
 if __name__ == "__main__":
 
@@ -67,7 +75,7 @@ if __name__ == "__main__":
     # app1.exec_()
 
     t = Thread(target=run_kiwoom_app)
-    t.daemon = True
+    t.daemon = True  # 백그라운드 스레드로 실행
     t.start()
 
     run_flask_app()
