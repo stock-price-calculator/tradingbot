46bdd3f2f5819b52d2f4d7acc9c29fc9744a4bf9
rong5026 <rong5026@naver.com>
2023-06-06 18:23:15 +0900

    [ADD] flask 주식구매

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import sys
 from PyQt5.QtWidgets import *
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify, request
 import constants
 from kiwoom import Kiwoom
 from threading import Thread
 from router.user import user_bp
+from account.account_sender import Kiwoom_Send_Account
+from order.trade import Kiwoom_Trade
+from market.stick_data_sender import Kiwoom_Price
 
 app = Flask(__name__)
 
 kiwoom = None
+kiwoom_account = None
+kiwoom_trade = None
+kiwoom_price = None
 
 # 처음 로그인 요청
 @app.route("/login")
@@ -19,7 +25,6 @@ def get_login():
         kiwoom = Kiwoom()
     kiwoom.connect_login()
     return jsonify({'result': True})
-
 
 # 유저 이름, 계좌, id
 @app.route("/user/data")
@@ -39,16 +44,14 @@ def get_user_data():
     else:
         return jsonify({"id": id, "name" : name, "account" : account})
 
+@app.route("/order/buy", methods=['POST'])
+def send_buy_order():
+    # data = request.json
+    # print(data['account'], data['item_code'], data['quantity'],data['price'], data['trading_type'])
 
+    buy_order = kiwoom_trade.send_buy_order(constants.ACCOUNT, constants.LG_CODE, 1, 0, "시장가")
 
-
-
-
-#####################################################################
-# 매수매도
-
-
-
+    return jsonify(buy_order)
 
 # Flask 서버 실행
 def run_flask_app():
@@ -57,9 +60,15 @@ def run_flask_app():
 # Kiwoom 서버 실행
 def run_kiwoom_app():
     global kiwoom
+    global kiwoom_account
+    global kiwoom_trade
+    global kiwoom_price
 
     app1 = QApplication(sys.argv)  # QApplication 인스턴스 생성
     kiwoom = Kiwoom()
+    kiwoom_account = Kiwoom_Send_Account(kiwoom)
+    kiwoom_trade = Kiwoom_Trade(kiwoom)
+    kiwoom_price = Kiwoom_Price(kiwoom)
     app1.exec_()
 
 
